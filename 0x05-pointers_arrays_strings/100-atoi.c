a93624290679b92d0225be1a76bf18c82967c806
stansmile <stansmile35@gmail.com>
2022-09-20 14:17:34 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,7 +15,7 @@ int _atoi(char *s)
 		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
 
-		
+
 		if (*(s + count) == '-')
 			k *= -1;
 		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
